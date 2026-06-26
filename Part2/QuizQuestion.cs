ee27918cca856c3f8a7c56f62e928566a21dfbb6
Mikael082211 <st10474442@rcconnect.edu.za>
2026-06-26 21:02:23 +0200

    Add files via upload
    I also added a QuizQuestion.cs

---
+++ Part2/QuizQuestion.cs
@@ -0,0 +1,14 @@
+﻿namespace Part2
+{
+    public class QuizQuestion
+    {
+        public string Question { get; set; }
+        public string Answer { get; set; }
+
+        public QuizQuestion(string q, string a)
+        {
+            Question = q;
+            Answer = a;
+        }
+    }
+}
