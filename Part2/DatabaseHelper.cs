8ad46fa7a9b661fd7704578414c4a7bcd6b6a820
Mikael082211 <st10474442@rcconnect.edu.za>
2026-06-26 21:00:30 +0200

    Add files via upload
    I added a DatabaseHelper.cs

---
+++ Part2/DatabaseHelper.cs
@@ -0,0 +1,15 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace Part2
+{
+    public static class DatabaseHelper
+    {
+        private static string connectionString =
+            "server=localhost;database=CyberBot;uid=root;pwd=password;";
+
+        public static MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(connectionString);
+        }
+    }
+}
