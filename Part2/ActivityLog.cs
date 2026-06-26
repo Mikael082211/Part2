df54f4695798b1956b39d44df6972fdddff304a3
Mikael082211 <st10474442@rcconnect.edu.za>
2026-06-26 20:55:09 +0200

    Add files via upload

---
+++ Part2/ActivityLog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Part2
+{
+    public static class ActivityLog
+    {
+        public static List<string> Logs = new();
+
+        public static void Add(string message)
+        {
+            Logs.Add($"{DateTime.Now:HH:mm:ss} - {message}");
+        }
+    }
+}
